ed537510aa846df3d7462ec192657d71307cade8
DinosaurDinoko <mega.lelya1235@gmail.com>
2023-02-14 11:39:06 +0700

    PR_5

--- vue/js/main.js
+++ vue/js/main.js
@@ -27,9 +27,10 @@ let app = new Vue({
         updateProduct(variantImage) {
             this.image = variantImage
         },
-
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
-
 })
 
 
